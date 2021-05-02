f22ca489d2d334e15548abaf65a028c8508dc638
bryanmmr <bryanmmr@gmail.com>
2021-05-01 23:42:51 -0500

    Google maps api

--- clima/views.py
+++ clima/views.py
@@ -7,11 +7,13 @@ import json
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',PE&units=metric&appid=' + API_KEY).read()
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + ',PE&units=metric&appid=' + API_KEY + '&lang=es').read()
 
         list_of_data = json.loads(source)
         data = {
             "country_code" : str(list_of_data['sys']['country']),
+            "lat" : str(list_of_data['coord']['lat']),
+            "lng" : str(list_of_data['coord']['lon']),
             "coordinate" : str(list_of_data['coord']['lat']) + str(list_of_data['coord']['lon']),
             "temp"  : str(list_of_data['main']['temp']) + 'C',
             "pressure" : str(list_of_data['main']['pressure']),
@@ -19,6 +21,7 @@ def index(request):
             "main"  : str(list_of_data['weather'][0]['main']),
             "description" : str(list_of_data['weather'][0]['description']),
             "icon" : str(list_of_data['weather'][0]['icon']),
+            "API_KEY" : str(API_KEY_MAPS),
         }
         print(data)
     else:
